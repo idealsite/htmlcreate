3efdaf4deb5fdb4800962c5b16ebec12abeb2770
Idealsite <idealsite@idealsite.it>
2010-08-19 01:03:04 +0200

    bah, im bad in shell scriptin

--- silus-script.sh
+++ silus-script.sh
@@ -1,26 +1,23 @@
-clear
-frase_uno=(Hi! Whats your name?)
-frase_due=(Omg $inputname! Your name sucks!)
-echo $fras[1]
-function scrivi(){
-	frase=$1
-	for ((  i = 0 ;  i < ${#frase[*]};  i++  )) ; do
-		parola="${frase[$i]}"
-		lunghezza=`expr length $parola`
-		ultimaLettera=`expr $lunghezza - 1`
-		for ((  k = 0 ;  k < ${#parola};  k++  )) ; do
-			if [ "$k" -eq "$ultimaLettera" ] ; then
-				echo -n "${parola:$k:1} "
-				sleep 0.1
-			else
-				echo -n "${parola:$k:1}"
-				sleep 0.1
-			fi
-		done
-	done
-	return
-}
 
-scrivi $frase_uno
-read inputname
+clear
+frase=(modafucka aaa)
+
+wordCount=${#frase}
+lastWord=`expr $wordCount - 1`
+for (( i = 0 ; i < $wordCount ; i++ )) ; do
+	word=${frase[$i]}
+	wordLength=`expr length $word`
+	lastLetter=`expr $wordLength - 1`
+	for (( j = 0 ;  j < $wordLength ; j++ )) ; do
+		echo -n "${word:$j:1}"
+		sleep 0.1
+		if [ "$j" -eq "$lastLetter" ] ; then
+			echo -n " "
+		fi
+	done
+	if [ "$i" -eq "$lastWord" ] ; then
+		echo
+	fi
+done
 exit 0
+
