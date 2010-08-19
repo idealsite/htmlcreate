372a46327a07ef0f5f1877e653054bf8af00555a
Idealsite <idealsite@idealsite.it>
2010-08-19 21:24:16 +0200

    moved and added functions

--- silus-script.sh
+++ silus-script.sh
@@ -1,21 +1,5 @@
 
 clear
-
-writePhrase () {
-	phrase="$1"
-	phraseLength="${#phrase}"
-	lastLetter=`expr "$phraseLength" - 1`
-	for (( i = 0 ; i < "$phraseLength" ; i++ )) ; do
-		letter="${phrase:$i:1}"
-		echo -n "$letter"
-		sleep 0.1
-		if [ "$i" -eq "$lastLetter" ] ; then
-			echo
-		fi
-	done
-}
-
-writePhrase "what the hell is going on?"
-
+askName
 exit 0
 
